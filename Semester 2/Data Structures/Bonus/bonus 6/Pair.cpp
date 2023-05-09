3fe7c4cce00d7a23f18a1dd0b218b074a06da5f5
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-05-09 20:38:16 +0300

    Bonus 6 add Pair.cpp

---
+++ Semester 2/Data Structures/Bonus/bonus 6/Pair.cpp
@@ -0,0 +1,37 @@
+// definition of class Pair
+
+// includes
+#include "Pair.h"
+
+// class
+template <class X> Pair <X>:: Pair(){   // default constructor
+    a=0;
+    b=0;
+}
+
+template <class X> Pair <X>:: Pair(X a, X b){   // constructor with 2 parameters
+    this->a = a;
+    this->b = b;
+}
+
+template <class X> void Pair <X>:: setFirst(X a){   // setter for first var
+    this->a = a;
+}
+
+template <class X> void Pair <X>:: setFirst(X b){   // setter for second var
+    this->b = b;
+}
+
+template <class X> X Pair <X>:: getFirst(){   // setter for first var
+    return(a);
+}
+
+template <class X> X Pair <X>:: getSecond(){   // setter for second var
+    return(b);
+}
+
+template <class X> void Pair <X>:: swap(){   // swaps the 2 vars' values
+    temp = a;
+    a = b;
+    b = temp;
+}
