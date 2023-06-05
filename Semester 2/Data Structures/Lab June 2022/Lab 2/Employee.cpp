df3bb76d59cefd930e14d9d4d2684be8ef9e38e4
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-06-05 12:58:56 +0300

    Delete useless files

--- Semester 2/Data Structures/Lab June 2022/Lab 2/Employee.cpp
+++
@@ -1,9 +0,0 @@
-#include "Employee.h"
-
-Employee:: Employee(std::string surname) {
-    this->surname = surname;
-}
-
-std::string Employee:: getSurname() {
-    return surname;
-}
--- Semester 2/Data Structures/Lab June 2022/Lab 2/Professor.cpp
+++
@@ -1,9 +0,0 @@
-#include "Professor.h"
-
-Professor:: Professor(std::string surname) : Employee(surname) {}
-
-int Professor:: getPublishes() {return publishes;}
-
-void Professor:: setPublishes(int publishses) {
-    this->publishes = publishes;
-}
