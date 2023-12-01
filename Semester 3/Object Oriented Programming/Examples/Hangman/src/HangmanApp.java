ccee878956c899792636294b82fff9bf813c9bba
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-12-01 19:20:48 +0200

    First working sample.

--- Semester 3/Object Oriented Programming/Examples/Hangman/src/HangmanApp.java
+++ Semester 3/Object Oriented Programming/Examples/Hangman/src/HangmanApp.java
@@ -1,5 +1,39 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class HangmanApp {
     public static void main(String[] args) {
-        HangmanGame hangmanGame = new HangmanGame();
+        Scanner scanner = new Scanner(System.in);
+        int gameMode;
+
+        System.out.println("Select Game Mode:");
+        System.out.println("1. SinglePlayer");
+        System.out.println("2. MultiPlayer");
+
+        while (true) {
+            try {
+                gameMode = scanner.nextInt();
+                if (gameMode == 1 || gameMode == 2) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter 1 or 2.");
+                }
+            } catch (InputMismatchException e) {
+                scanner.nextLine(); // Consume the invalid input
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+        }
+
+        HangmanGame hangmanGame;
+        if (gameMode == 1) {
+            hangmanGame = new HangmanGame();
+            hangmanGame.playGame();
+        } else {
+            System.out.println("Enter the mysterious word:");
+            String hiddenWord = scanner.nextLine();
+            scanner.close();
+            hangmanGame = new HangmanGame(hiddenWord);
+            hangmanGame.playGame();
+        }
     }
 }
--- Semester 3/Object Oriented Programming/Examples/Hangman/src/HangmanGame.java
+++ Semester 3/Object Oriented Programming/Examples/Hangman/src/HangmanGame.java
@@ -1,32 +1,92 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class HangmanGame {
     private String hiddenWord;
-    private ArrayList<String> foundLetters;
+    private Character[] foundLetters;
+    private ArrayList<Character> lettersLeft;
     private int lives;
-    private String[] lettersLeft = {
-            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-    };
+
 
     // Default constructor
     public HangmanGame() {
-        this.lives = 6;
+        lives = 6;
         initializeGame(new WordGenerator().generateRandomWord());
     }
 
     // Parameterized constructor
     public HangmanGame(String hiddenWord) {
-        this.lives = 6;
+        lives = 6;
         initializeGame(hiddenWord);
     }
 
     private void initializeGame(String hiddenWord) {
         this.hiddenWord = hiddenWord;
 
-        foundLetters = new ArrayList<>();
-        for (char c : hiddenWord.toCharArray()) {
-            foundLetters.add("_");
+        foundLetters = new Character[hiddenWord.length()];
+        for (int i=0 ; i<hiddenWord.length() ; i++) {
+            foundLetters[i] = '_';
         }
+
+        lettersLeft = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            lettersLeft.add((char) ('a' + i));
+        }
+    }
+
+    public void playGame() {
+        while (lives > 0 && Arrays.asList(foundLetters).contains('_')) {
+            System.out.println("Lives left: " + lives);
+
+            // Print current state
+            for (Character foundLetter : foundLetters) {
+                System.out.print(foundLetter);
+            }
+            System.out.println();
+
+            // Print available letters
+            System.out.println("You can choose from the following letters");
+            for (Character s : lettersLeft) {
+                System.out.print(s + " ");
+            }
+            System.out.println("\n" + "Take a guess: ");
+
+            // Guess a character
+            Scanner scanner = new Scanner(System.in);
+            Character chosenLetter;
+            while (true) {
+                try {
+                    chosenLetter = scanner.next().charAt(0);
+                    if (lettersLeft.contains(chosenLetter)) {
+                        lettersLeft.remove(chosenLetter);
+                        break;
+                    } else {
+                        System.out.println("You cannot choose this character... Try again:");
+                    }
+                } catch (InputMismatchException e) {
+                    scanner.nextLine(); // Consume the invalid input
+                    System.out.println("Invalid Input... Please choose a valid character:");
+                }
+            }
+
+            int i = 0;
+            boolean found = false;
+            for (Character c : hiddenWord.toCharArray()) {
+                if (c == chosenLetter) {
+                    foundLetters[i] = chosenLetter;
+                    found = true;
+                }
+                i++;
+            }
+            if (!found) {lives--;}
+        }
+        if (lives > 0) {
+            System.out.println("Congratulations, you won!");
+        } else {
+            System.out.println("You lost... better luck next time:)");
+        }
+        System.out.println("The word was " + hiddenWord);
     }
 }
