13f2bc2748f581c3bc6acf42e90b4e6fd9b4d9cd
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-12-01 18:03:10 +0200

    Add basic methods to HangmanGame class.

--- Semester 3/Object Oriented Programming/Examples/Hangman/src/HangmanGame.java
+++ Semester 3/Object Oriented Programming/Examples/Hangman/src/HangmanGame.java
@@ -1,16 +1,32 @@
+import java.util.ArrayList;
+
 public class HangmanGame {
     private String hiddenWord;
+    private ArrayList<String> foundLetters;
     private int lives;
+    private String[] lettersLeft = {
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    };
 
+    // Default constructor
     public HangmanGame() {
-        lives = 6;
-        WordGenerator wordGenerator = new WordGenerator();
-        hiddenWord = wordGenerator.generateRandomWord();
-        System.out.println(hiddenWord);
+        this.lives = 6;
+        initializeGame(new WordGenerator().generateRandomWord());
     }
 
+    // Parameterized constructor
     public HangmanGame(String hiddenWord) {
-        lives = 6;
+        this.lives = 6;
+        initializeGame(hiddenWord);
+    }
+
+    private void initializeGame(String hiddenWord) {
         this.hiddenWord = hiddenWord;
+
+        foundLetters = new ArrayList<>();
+        for (char c : hiddenWord.toCharArray()) {
+            foundLetters.add("_");
+        }
     }
 }
